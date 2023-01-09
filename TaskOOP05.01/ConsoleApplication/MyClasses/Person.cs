e6197690db5998cf5edf99be4e4b0161782248be
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-09 17:24:04 +0300

    TaskOOP05.01

---
+++ TaskOOP05.01/ConsoleApplication/MyClasses/Person.cs
@@ -0,0 +1,13 @@
+// Классы Person и Customer. 
+//+Напишите класс Person с атрибутами данных для
+// +имени, адреса и телефонного номера человека. 
+
+
+namespace PersonWork;
+
+public class Person
+{
+    public string Name;
+    public string Adress;
+    public string Tel;
+}
