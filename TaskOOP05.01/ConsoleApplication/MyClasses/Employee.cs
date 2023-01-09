6a93c14bdc116d997aa84039bd9621168fd52f45
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-09 17:22:41 +0300

    TaskOOP05.01

---
+++ TaskOOP05.01/ConsoleApplication/MyClasses/Employee.cs
@@ -0,0 +1,17 @@
+// Классы Employee и ProductionWorker. 
+// + Напишите класс Ernployee (Сотрудник),
+// который содержит атрибуты приведенных ниже данных:
+// +• имя сотрудника;
+// +• номер сотрудника.
+
+
+namespace EmployeeWork;
+
+public class Employee
+{
+    public string Name;
+    public int Id;
+
+
+
+}
