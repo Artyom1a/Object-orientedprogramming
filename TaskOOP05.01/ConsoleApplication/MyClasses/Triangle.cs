8191f8d157ab1126a805fdfe3e5dd51019251de1
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-10 18:46:50 +0300

    TaskOOP05.01

---
+++ TaskOOP05.01/ConsoleApplication/MyClasses/Triangle.cs
@@ -0,0 +1,22 @@
+namespace FigureWork;
+
+public class Triangle : Figure
+{
+    private double SideA;
+    private double SideB;
+
+    private double SideC;
+
+    public Triangle(string name, double sideA, double sideB, double sideC) : base(name)
+    {
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+    public override void Square()
+    {
+        double p = (SideA + SideB + SideC) / 2;
+        double square = Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+        System.Console.WriteLine($"Rectangle {Name} square = {square}");
+    }
+}
