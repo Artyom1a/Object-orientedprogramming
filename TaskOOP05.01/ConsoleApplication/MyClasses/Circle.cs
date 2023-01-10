3d43e3d617e2c58516d7b53ff82ea318577bd7c0
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-10 18:47:22 +0300

    TaskOOP05.01

---
+++ TaskOOP05.01/ConsoleApplication/MyClasses/Circle.cs
@@ -0,0 +1,27 @@
+// // Реализовать следующие классы Круг, Треугольник и Квадрат. Каждый экземпляр
+// // +класса должен содержать свойства Площадь, Периметр и Имя (свойства
+// // родительского класса). Создвать массив из фигур и вывести всю информацию о
+// // фигурах в консоль. Площадь и периметр должны рассчитываться по
+// // математическим формулам
+
+// using FigureWork;
+// namespace CircleWork;
+
+// public class Circle : Figure
+// {
+//     public double P = 3.14;
+//     public double R;
+//     public Circle(string name, double perimeter, double area, double r) : base(name, perimeter, area)
+//     {
+//         Name = name;
+//         Perimeter = perimeter;
+//         Area = area;
+//         R = r;
+//     }
+//     public void PlosCircule()
+//     {
+//         Area = P * R * R;
+//         System.Console.WriteLine(Area);
+//     }
+
+// }
