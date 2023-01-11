830abcbd1eb70051ced0babe32f5d6f139703514
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-11 10:35:56 +0300

    OOP10.01

---
+++ OOP10.01/MyClasses/Car.cs
@@ -0,0 +1,47 @@
+namespace MyClasses.Cars;
+
+public class Car : MyClasses.Transport.Transport
+{
+    private string _name;
+    public override string Name
+    {
+        get { return !string.IsNullOrEmpty(_name) ? _name : "Our Car"; }
+        set { _name = value; }
+
+    }
+
+    public int Type { get; set; }
+    public Car()
+    {
+
+    }
+
+    public Car(double spead, string name, int type) : base(spead, name)
+    {
+        Type = type;
+    }
+    public Car(string name, int type) : base(name)
+    {
+        Type = type;
+    }
+
+    public Car(string name) : base(name)
+    {
+
+    }
+
+
+    public void Drift()
+    {
+        System.Console.WriteLine($"{Name} is breaken");
+    }
+
+    public override void Moving()
+    {
+        base.Moving();
+        System.Console.WriteLine($"Car {Name} is moving");
+    }
+
+
+
+}
