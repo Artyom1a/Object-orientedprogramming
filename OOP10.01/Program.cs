4a894f293128a4e8dbda123db38f9c15f07a0e8e
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-11 10:35:30 +0300

    OOP10.01

---
+++ OOP10.01/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using MyClasses.Cars;
+using MyClasses.Transport;
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Transport transport = new Transport(20, "car");
+
+            // transport.Moving();
+            // // transport.Stopping();
+            // Car bugatti = new Car("vasy", 22);
+            // System.Console.WriteLine(bugatti.ToString());
+
+            // // bugatti.Drift();
+            // Transport lambo = new Car("artem", 22);
+
+            // Transport[] array = new Transport[3] { transport, bugatti, lambo };
+            // foreach (Transport item in array)
+            // {
+            //     System.Console.WriteLine(item.ToString());
+            //     if(item is Car car)
+            //     {
+            //         System.Console.WriteLine("This is car");
+            //         car.Drift();
+            //     }
+            //     if(item is Transport)
+            //     {
+            //        // System.Console.WriteLine("This is Sparta");
+            //     }
+            //     item.Stopping();
+            //     item.Moving();
+            // }
+            // System.Console.WriteLine("Transport class");
+            // Transport transport = new Transport();
+            // System.Console.WriteLine(transport.Name);
+            // transport.Moving();
+            // System.Console.WriteLine("Car class");
+            // Car car = new Car();
+            // System.Console.WriteLine(car.Name);
+            // car.Moving();
+            // System.Console.WriteLine("Transport-car class");
+            // Transport transportCar = new Car();
+            // System.Console.WriteLine(transportCar.Name);
+            // transportCar.Moving();
+        }
+    }
+}
