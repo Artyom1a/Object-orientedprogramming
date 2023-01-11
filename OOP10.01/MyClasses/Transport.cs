cce4c2d50ae19122d268988c8c051107f9cf5466
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-11 10:36:10 +0300

    OOP10.01

---
+++ OOP10.01/MyClasses/Transport.cs
@@ -0,0 +1,37 @@
+namespace MyClasses.Transport;
+public class Transport
+{
+
+    public double Spead;
+    public virtual string? Name { get; set; }
+
+    public Transport()
+    {
+        
+    }
+
+    public Transport(double spead, string name) : this(name)
+    {
+        Spead = spead;
+    }
+    public Transport(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void Moving()
+    {
+        System.Console.WriteLine($"Transport {Name} is moving");
+    }
+
+    public  void Stopping()
+    {
+        System.Console.WriteLine($"{Name} is stopping");
+    }
+
+    public override string ToString()
+    {
+        return $"{Name},{Spead}";
+
+    }
+}
