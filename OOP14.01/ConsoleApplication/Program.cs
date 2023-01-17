87b811cc577119645a294609a0581c5b38d76f0a
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-17 15:11:15 +0300

    OOP14.01

---
+++ OOP14.01/ConsoleApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿// using Validations;
+// using Users;
+using Cards;
+using MyClasses;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data data = new Data();
+            // System.Console.WriteLine(data.IsValidEmail("gre@gmail.com"));
+            // Server server = new Server(1,"RTY","gwve42");
+            // System.Console.WriteLine(server.GetUser());
+            Card card = new Card("1");
+            // card.TryGetMoney(100);
+            // card.PushMoney(100);
+            // card.TryGetMoney(100);
+
+            // System.Console.WriteLine((int)WalletTypes.Crypto);
+            // System.Console.WriteLine((int)WalletTypes.Leather);
+        
+            // wallet1.PushMoney(5);
+
+            // Shop shop = new Shop();
+            // shop.Pay(wallet1);
+            // shop.Pay(card);
+
+            IPayment wallet = new LeatherWallet(10);
+            wallet.PushMoney(10);
+            
+
+
+        }
+    }
+}
+
+
+
