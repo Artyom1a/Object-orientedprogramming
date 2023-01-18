b7b1333b72594f88c7d1fb7a0f814c95c0075197
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-18 15:14:32 +0300

    OOP14.01

---
+++ OOP14.01/ConsoleApplication/MyClasses/Shop.cs
@@ -0,0 +1,22 @@
+using Cards;
+namespace MyClasses;
+
+
+public class Shop
+{
+    public decimal Money { get; set; }
+    public void Pay(IPayment item)
+    {
+        if (decimal.TryParse(Console.ReadLine(), out decimal sum))
+        {
+            if (item.TryGetMoney(sum))
+            {
+                Money += sum;
+            }
+            else
+            {
+                System.Console.WriteLine("Not enough money");
+            }
+        }
+    }
+}
