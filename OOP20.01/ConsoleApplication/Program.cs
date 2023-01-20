4459f5b179ce643a1375886150b178be715835c1
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-20 15:41:38 +0300

    OOP20.01

---
+++ OOP20.01/ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using Customers;
+
+namespace ProgramCustomer;
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Customer customer = new Customer("Svet","egr");
+        customer.CallCustomer();
+    }
+}
+
