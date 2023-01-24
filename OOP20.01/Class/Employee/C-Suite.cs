9c16e5c7107f5c80acffd10d47fd846b5c3ca318
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-24 21:15:16 +0300

    OOP20.01

---
+++ OOP20.01/Class/Employee/C-Suite.cs
@@ -0,0 +1,28 @@
+namespace Works;
+
+public class CSuite : IManagementLevelEmployee, ICSuite_Privilege
+{
+    public string Name
+    {
+        get;
+        set;
+    }
+    public string Designation
+    {
+        get;
+        set;
+    }
+    public IEvaluatedEmployee EvaluateSubordinate(IEvaluatedEmployee Manager)
+    {
+        Random random = new Random();
+        Manager.KPI = random.Next(60, 100);
+        return Manager;
+    }
+    public bool TerminateEmployee(IEvaluatedEmployee evemp)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Employee {evemp.Name} with KPI {evemp.KPI} has been terminated because of KPI below 70");
+        Console.ForegroundColor = ConsoleColor.White;
+        return true;
+    }
+}
