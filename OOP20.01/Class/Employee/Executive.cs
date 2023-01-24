032dfe74aefca478078bd8f6fb968f66ab821db2
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-24 21:15:30 +0300

    OOP20.01

---
+++ OOP20.01/Class/Employee/Executive.cs
@@ -0,0 +1,20 @@
+namespace Works;
+
+public class Executive : IEvaluatedEmployee
+{
+    public string Name
+    {
+        get;
+        set;
+    }
+    public string Designation
+    {
+        get;
+        set;
+    }
+    public int KPI
+    {
+        get;
+        set;
+    }
+}
