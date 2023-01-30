149517638ea759751d7c95f84243e4f60233f686
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-30 23:45:45 +0300

    OOP31.01

---
+++ OOP3101/TaskExtencions/Program.cs
@@ -0,0 +1,21 @@
+﻿using TaskExtencions.Enums;
+
+namespace TaskExtencions.Extencions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = "ewfewv324gervbw92";
+            //int result = text.ToInt(); 
+            //Console.WriteLine(result);
+
+            //object test = null;
+            //Console.WriteLine(test.TryToConvert<int>(out int result));
+            //Console.WriteLine(result);
+
+            Console.WriteLine(((FileTypes)3).FileTypeMessage());
+
+        }
+    }
+}
