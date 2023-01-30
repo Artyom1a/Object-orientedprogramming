8c0c097dee65182a7478241d8da137544de7e253
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-30 23:46:46 +0300

    OOP31.01

---
+++ OOP3101/TaskExtencions/Enums/FileTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExtencions.Enums
+{
+    public enum FileTypes
+    {
+        pdf =1, txt =2, doc =3, html =4, svg =5,
+    }
+}
+
+//+Создать перечисление содержащая кода файлов PDF = 1, TXT = 2 и тд. 
