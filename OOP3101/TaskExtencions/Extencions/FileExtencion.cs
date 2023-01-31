880be4f8e054b2a268632caf321b6127230e8a61
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-31 12:47:29 +0300

    OOP31.01

---
+++ OOP3101/TaskExtencions/Extencions/FileExtencion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskExtencions.Enums;
+
+namespace TaskExtencions.Extencions
+{
+    public static class FileExtencion
+    {
+        public static string FileTypeMessage(this FileTypes x)
+        {
+            if (Enum.IsDefined(x))
+            {
+                return $"Выбран файл с расширением *.{x}";
+            }
+         
+            throw new Exception("Неверный формат файла");
+        }
+
+    }
+}
+
+//Написать метод
+//расширения, который в зависимости от значения перечисления будет возвращать
+//результат следующего вида: Выбран файл с расширением *. pdf”pdf”, если такого значения нет
+//в перечислении бросать ошибку с сообщением Неверный формат файла
