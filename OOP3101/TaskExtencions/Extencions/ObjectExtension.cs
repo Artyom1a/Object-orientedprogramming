118186e9f51186d2995db36ccca362edcb405493
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-01-31 12:47:48 +0300

    OOP31.01

---
+++ OOP3101/TaskExtencions/Extencions/ObjectExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExtencions.Extencions
+{
+    public static class ObjectExtension
+    {
+        public static bool TryToConvert<T>(this object obj, out T result)
+        {
+
+            if(obj is T temp)
+            {
+                result = temp;
+                return true;
+            }
+            result = default;
+            return false;
+
+        }
+    }
+}
+
+//Написать метод расширения типа object , который возращает true , если можно
+//преобразовать в указанный тип данных и преобразованное значение, в противном случае
+//false. Метод должен называться TryToConvert
