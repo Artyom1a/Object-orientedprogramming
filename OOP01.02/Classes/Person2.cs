9aceeaeac6b4703b23af38b69719d4f379792391
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-01 10:57:10 +0300

    OOP01.02

---
+++ OOP01.02/Classes/Person2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    class Person2 : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person2(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
