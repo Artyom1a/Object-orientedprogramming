6af45149527bde2d8fac509e7a9d2ed759f3d844
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-02 11:08:22 +0300

    OOP02.02

---
+++ OOP01.02/Classes/MyGenericClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    // Обобщенный класс
+    public class MyGenericClass<T>
+    {
+        // Обобщенные переменные
+        private T a, b;
+
+        // Конструктор
+        public MyGenericClass()
+        {
+            // Присвоение значения по умолчанию переменным a, b
+            a = default(T);
+            b = default(T);
+        }
+    }
+}
