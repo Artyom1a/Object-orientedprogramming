fdabc3d1ffb4dece873cddec111d5707025a5411
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-04 14:42:05 +0300

    OOP03.02

---
+++ OOP03.02/Modules/Money.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+public class Money
+{
+    
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string Symbol { get; set; }
+    [JsonPropertyName("d")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public DateTime? LastUpdate { get; set; }
+    [JsonIgnore]
+    public string Text => $"{Code}+{Name}";
+
+    public Money()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return $"{Code}   {Name}  {Symbol}";
+    }
+}
