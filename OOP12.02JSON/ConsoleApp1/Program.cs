6b329311a48f47fd67a09dae6623a3d309ecaa27
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-11 20:10:02 +0300

    OOP12.02

---
+++ OOP12.02JSON/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1.JSON;
+using System.Text.Json;
+
+
+namespace ConsoleApp1// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(1, "Test", DateTime.UtcNow, UserStatus.Admin);
+            //{
+            //    user.Id = 1;
+            //    user.Name = "Test";
+            //    user.LastUpdate = DateTime.UtcNow;
+            //    user.Status = UserStatus.Admin;
+            //}
+            Console.WriteLine("ToString");
+            Console.WriteLine(user.ToString());
+
+
+            string json = JsonSerializer.Serialize(user);
+            Console.WriteLine("JsonSerializer.Serialize");
+            Console.WriteLine(json);
+            
+            User newUser = JsonSerializer.Deserialize<User>(json);
+            Console.WriteLine("JsonSerializer.Deserialize");
+            Console.WriteLine(newUser);
+
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            string json1 = JsonSerializer.Serialize(user, serializeOptions);
+            Console.WriteLine("JsonSerializerOptions, JsonSerializer.Serialize");
+            Console.WriteLine(json1);
+
+            User newUser1 = JsonSerializer.Deserialize<User>(json, serializeOptions);
+            Console.WriteLine("JsonSerializerOptions, JsonSerializer.Deserialize");
+            Console.WriteLine(newUser1);
+        }
+
+       
+    }
+}
