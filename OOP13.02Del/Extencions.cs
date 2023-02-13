2c1a37fd98094a863d93f72e80fc8d393ee95f11
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-13 10:28:17 +0300

    OOP13.02

---
+++ OOP13.02Del/Extencions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Extencions;
+
+//Написать метод обобщения, которыйм производит фильтрацию данныхю В качестве
+//входжных параметров коллекция(данных обобщенного типа и делигат, по котолрому
+//будет производиться фильтрация данных.
+
+
+
+
+namespace ConsoleApp1
+{
+
+    internal static class Extencions
+    {
+       
+        public delegate bool Filter<T>(T item);
+        public static List<T> FilterList<T>(this List<T> data, Filter<T> method)
+        {
+            List<T> list = new List<T>();
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (method(data[i]))
+                {
+                    list.Add(data[i]);
+                }
+            }
+            return list;
+        }
+
+
+
+
+    }
+}
