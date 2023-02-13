51f690af4133c868774736faf7e4778fd3777e82
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-13 10:29:08 +0300

    OOP13.02

---
+++ OOP13.02Del/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Test
+    {
+        public int CustomCalculated(int number1, int number2, int number3)
+        {
+            return (number1 + number2) * number3;
+        }
+
+    }
+}
