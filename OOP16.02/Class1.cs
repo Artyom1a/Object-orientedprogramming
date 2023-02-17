ecfcdcbeb72a8f4b35be8a35f4c479c98d1eeb42
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-17 19:41:04 +0300

    oop17.02

---
+++ OOP16.02/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//дана строка
+//отсортировать слова в соответсвии с количеством гласных букв
+
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+    }
+}
