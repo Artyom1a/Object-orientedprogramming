730504756c591b575f767195e7574bde2d447998
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-17 19:39:09 +0300

    oop17.02

---
+++ OOP14.02Delegatdm/Task2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Task2
+    {
+        public double Pokaz(double x,double a, double b, double c)
+        {
+            double z = a * x * x + b * x + c;
+            return z;
+        }
+    }
+}
