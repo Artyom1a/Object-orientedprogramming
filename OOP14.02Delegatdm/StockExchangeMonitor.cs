c970b27dd75e1bfe7ecb60f335142b1af163093a
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-17 19:38:56 +0300

    oop17.02

---
+++ OOP14.02Delegatdm/StockExchangeMonitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class StockExchangeMonitor
+    {
+
+        public delegate void PriceChange(int price);
+
+        public PriceChange PriceChangeHandler { get; set; }
+
+        public void Start()
+        {
+            while(true)
+            {
+                int bankofamericansbank = (new Random().Next(0, 1000));
+                PriceChangeHandler(bankofamericansbank);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
