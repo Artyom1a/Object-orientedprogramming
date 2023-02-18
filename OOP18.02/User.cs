831ee79d45386082a45a932a4ec7ac61053b8719
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-18 13:43:11 +0300

    oop18.02

---
+++ OOP18.02/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Phone { get; set; }
+
+        public User(string name, int id, string phone) => (Name,Id, Phone)=(name,id,phone);
+        //{
+        //    Name = name;
+        //    Id = id;
+        //    Phone = phone;
+        //}
+    }
+}
