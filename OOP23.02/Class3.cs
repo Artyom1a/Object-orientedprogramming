fca762008b683108cc6bcd19ead2eac56018c034
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-22 08:41:30 +0300

    OOP23.02

---
+++ OOP23.02/Class3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class3
+    {
+
+        //Напишите программу, которая запрашивает 
+        //            //    у пользователя число и затем проверяет,
+        //            //    является ли оно палиндромом(то есть числом
+        //            //        , которое читается одинаково как справа
+        //            //        налево, так и слева направо).
+
+        //            string num = Console.ReadLine().Trim();
+        //            string ttr = string.Join("", num.Reverse());
+        //            if(num==ttr)
+        //            {
+        //                Console.WriteLine($"{num}- number is palindrom");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine($"{num}- number is not palindrom");
+
+        //            }
+    }
+}
