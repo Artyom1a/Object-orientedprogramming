14cbc9951bc9251f35ed92197bc649382ace0987
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-22 08:41:07 +0300

    OOP23.02

---
+++ OOP23.02/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+
+        //Напишите метод для нахождения наименьшего
+        ////            //    общего кратного двух чисел.Пользователь 
+        ////            //    должен вводить два числа, а программа 
+        ////            //    должна выводить на экран их наименьшее 
+        ////            ////    общее кратное.
+        ////            //string number1 = Console.ReadLine();
+        ////            //string number2 = Console.ReadLine();
+        ////            //if (int.TryParse(number1, out int value) && int.TryParse(number2, out int value1))
+        ////            //{
+        ////            //    Console.WriteLine(NOK(value, value1));
+        ////            //}
+        ////            //else
+        ////            //{
+        ////            //    Console.WriteLine();
+        ////            //}
+
+        ////            //Создайте приложение для нахождения суммы всех чисел от 1 до 1000, кратных 3 или 5.
+
+        ////            //Console.WriteLine(SumKr());
+        ///
+        ////        //static int SumKr()
+        ////        //{
+
+        ////        //    int result = 0;
+        ////        //    for (int i = 1; i <= 1000; i++)
+        ////        //    {
+        ////        //        if (i % 3 == 0 || i % 5 == 0)
+        ////        //        {
+        ////        //            result += i;
+        ////        //        }
+        ////        //    }
+        ////        //    return result;
+
+        ////        //}
+    }
+}
