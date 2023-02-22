e7c455b8266541cfbb56b8adc1aa98892e5b80ff
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-22 08:41:18 +0300

    OOP23.02

---
+++ OOP23.02/Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class2
+    {
+
+        //Создайте метод расширения для определения 
+        ////            //    среднего значения интервала массива / 
+        ////            //    коллекции чисел.Пользователь должен 
+        ////            //    вводить индексы начала и конца интервала
+        ////            //    для подсчёта среднего значения. 
+
+        ////            Console.WriteLine("length aaray:");
+        ////            string input1 = Console.ReadLine();
+        ////            if (!int.TryParse(input1, out int result)) throw new Exception("error");
+        ////            int[] array = new int[result];
+        ////            Random rnd = new Random();
+        ////            for (int i = 0; i < array.Length; i++)
+        ////            {
+        ////                array[i] = rnd.Next(0, 100);
+        ////            }
+        ////            Console.WriteLine("strart index");
+        ////            string input2 = Console.ReadLine();
+        ////            Console.WriteLine("end index");
+        ////            string input3 = Console.ReadLine();
+        ////            if(!int.TryParse(input1, out int result1) || !int.TryParse(input1, out int result2))  throw new Exception("error");
+        ////            double x = Avaredge(array, result1, result2);
+        ////            Console.WriteLine(x);
+        ////        }
+        ///
+
+        ////        public static double Avaredge(int[] array,int startIndex, int endIndex)
+        ////        {
+        ////            if (0 > startIndex || startIndex > endIndex || array.Length <= endIndex) throw new Exception("error");
+
+        ////            double temp = 0;
+        ////            double count = 0;
+        ////            for(int i= startIndex;i<=endIndex;i++)
+        ////            {
+        ////                temp += array[i];
+        ////                count++;
+        ////            }
+        ////            return temp / count;
+        ////        }
+        ////    }
+        ////}
+
+    }
+}
