b18afd3e9a8b9a20d9cd8731668c4be54e1a554c
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-22 08:41:58 +0300

    OOP23.02

---
+++ OOP23.02/Class4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class4
+    {
+
+        //            //Напишите функцию, которая принимает на вход два массива целых чисел и возвращает новый массив, 
+        //            //    содержащий элементы обоих массивов без повторений.
+
+        //            int[] array1 = { 10, 2, 3,33 };
+        //            int[] array2 = { 11, 2, 3,44 };
+        //            List<int> list = ArrayWithoutCopy(array1,array2);
+        //            Console.WriteLine(string.Join(" ",list));
+
+
+        //        }
+
+        //        public static List<int> ArrayWithoutCopy(int[] array1, int[] array2)
+        //        {
+        //            List<int> list = new List<int>();
+        //            for(int i=0;i<array1.Length;i++) 
+        //            {
+        //                if (!list.Contains(array1[i]))
+        //                {
+        //                    list.Add(array1[i]);
+        //                }
+        //            }
+        //            for (int i = 0; i < array2.Length; i++)
+        //            {
+        //                if (!list.Contains(array2[i]))
+        //                {
+        //                    list.Add(array2[i]);
+        //                }
+        //            }
+        //            return list;
+        //        }
+
+
+        //    }
+    }
+}
