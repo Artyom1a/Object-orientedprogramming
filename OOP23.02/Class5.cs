c277840d21252aac00b42d274cd2ff6016777446
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-02-23 21:58:34 +0300

    OOP23.02

---
+++ OOP23.02/Class5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class5
+    {
+
+        //Создайте приложение для проверки, является ли 
+        //    заданное число простым. Пользователь должен 
+        //    вводить число, а программа должна выводить 
+    //    //    на экран сообщение о том, является ли оно 
+    //    //    простым.
+    //    int x = 0;
+
+    //}
+
+    //public static bool SimpleNum(int num)
+    //{
+    //    for (int i = 2; i < num / 2; i++)
+    //    {
+    //        if (num % i == 0) return false;
+    //    }
+    //    return true;
+    //}
+}
+}
