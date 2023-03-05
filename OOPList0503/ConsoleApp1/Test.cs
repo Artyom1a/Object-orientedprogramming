e0cfed36dbcb9ee9c6c1f3af2d960835b71edd3f
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-05 22:16:44 +0300

    OOPList0503

---
+++ OOPList0503/ConsoleApp1/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Test
+    {
+        //объект List типизируется типом string. 
+        public string Name;
+        public Test(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
