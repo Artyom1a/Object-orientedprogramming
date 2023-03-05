2782512a59de196ab777133298bb53d149b3f456
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-05 22:16:13 +0300

    OOPList0503

---
+++ OOPList0503/ConsoleApp1/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee : Person
+    {
+        public string Company { get; set; }
+
+        public Employee(string company, string name) : base(name)
+        {
+            Company = company;
+
+        }
+        public sealed override void Print()  //new
+        {
+            Console.WriteLine($"{Name}{Company}");
+        }
+    }
+
+}
+
