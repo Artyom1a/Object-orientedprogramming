7a3863f121125fb3c62141924a0224a6570ea4cb
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-08 14:52:29 +0300

    08.03

---
+++ OOP01.03Extensions/ConsoleApp1/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class StringExtension
+    {
+        public static string DecoratorSymbolAfterDigit(this string str, char symbol)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(char ch in str)
+            {
+                sb.Append(ch);
+                if (char.IsDigit(ch)) ;
+                {
+                    sb.Append(symbol);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
