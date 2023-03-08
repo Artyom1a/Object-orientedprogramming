84b13dc4b2b1bd1a312ab0c9de4ae9352a1974ec
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-08 14:52:18 +0300

    08.03

---
+++ OOP01.03Extensions/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConsoleApp1;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "3f4435kjdbl56g65grdvlkb6hbry";
+            string newText = text.DecoratorSymbolAfterDigit('$');
+            Console.WriteLine(newText);
+        }
+    }
+}
+
+
+
+
+
