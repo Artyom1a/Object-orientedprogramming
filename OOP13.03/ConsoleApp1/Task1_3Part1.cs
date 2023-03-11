0f8fae6b74d4eb62411140a32c8ea69e567b0e68
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-11 21:49:39 +0300

    11.03

---
+++ OOP13.03/ConsoleApp1/Task1_3Part1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Task1_3Part1
+    {
+        public void Task2(int a) 
+        {
+        if(a%2==0)
+            {
+                Console.WriteLine("This is an even number.");
+            }
+        else
+            { Console.WriteLine("This is not an even number."); }
+        }
+        public void Task3(int a, int b)
+        {
+
+            Console.WriteLine((a+b)/2);
+
+        }
+
+    }
+}
