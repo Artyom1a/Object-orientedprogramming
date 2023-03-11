65dbe710c954dc0acd62b1e7080f1bb9ab4ec915
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-11 21:49:20 +0300

    11.03

---
+++ OOP13.03/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using ConsoleApp1;
+using System;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part1
+            // 1. Напишите программу, которая вычисляет сумму двух чисел, введенных
+            //пользователем.
+            //Console.WriteLine("Task1");
+            //string a = Console.ReadLine();
+            //string f = Console.ReadLine();
+            //bool b = int.TryParse(a, out int c);
+            //bool d = int.TryParse(f, out int e);
+            //if(b && d && c != null && e != null )
+            //{
+            //    Console.WriteLine(c+e);
+            //}
+
+            //2.Напишите программу, которая определяет, является ли введенное число четным
+            //или нечетным.
+
+            //Console.WriteLine("Task2");
+            //Task1_3Part1 task1_3Part1 = new Task1_3Part1();
+            //string a = Console.ReadLine();
+            //bool b = int.TryParse(a, out int c);
+            //if (b  && c != null )
+            //{
+            //    task1_3Part1.Task2(c);
+            //}
+
+            //3. Напишите программу, которая находит среднее арифметическое двух чисел,
+            //введенных пользователем.
+            //Console.WriteLine("Task3");
+            //Task1_3Part1 task1_3Part1 = new Task1_3Part1();
+            //string a = Console.ReadLine();
+            //string f = Console.ReadLine();
+            //bool b = int.TryParse(a, out int c);
+            //bool d = int.TryParse(f, out int e);
+            //if (b && d && c != null && e != null)
+            //{
+            //task1_3Part1.Task3(c, e);
+            //}
+            //4. Напишите программу, которая проверяет, является ли введенная строка
+            //палиндромом(слово или фраза, которые читаются одинаково слева направо и
+            //справа налево).
+            //Console.WriteLine("Task4");
+
+            //Task4_6Part1 task4_6Part1 = new Task4_6Part1();
+            //string a = Console.ReadLine();
+            //if (string.IsNullOrEmpty(a))
+            //{
+            //    task4_6Part1.Task4(a);
+            //}
+            
+        }
+    }
+}
