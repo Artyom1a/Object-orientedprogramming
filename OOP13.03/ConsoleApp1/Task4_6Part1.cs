511ecb47fe19dadbf8ab793c8ffbede787d059e3
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-11 21:49:49 +0300

    11.03

---
+++ OOP13.03/ConsoleApp1/Task4_6Part1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Task4_6Part1
+    {
+        public void Task4(string a)
+        {
+            string b="";
+            for (int i=a.Length-1; i>=0; i++) 
+            {
+                b += a[i];
+            }
+
+        if(a==b)
+            {
+                Console.WriteLine("This is polindrom");
+            }
+        else
+            { Console.WriteLine("This is not polindrom"); }
+        }
+    }
+}
