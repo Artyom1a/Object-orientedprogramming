b35185ca87e49cce244a0b6ebe1191afc0f857af
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-12 11:39:45 +0300

    12.03

--- OOP13.03/ConsoleApp1/Program.cs
+++ OOP13.03/ConsoleApp1/Program.cs
@@ -45,10 +45,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
             //{
             //task1_3Part1.Task3(c, e);
             //}
-            //4. Напишите программу, которая проверяет, является ли введенная строка
+            //4. ПЕРЕПУТАЛ они в другой очередности Напишите программу, которая проверяет, является ли введенная строка
             //палиндромом(слово или фраза, которые читаются одинаково слева направо и
             //справа налево).
-            //Console.WriteLine("Task4");
+            //Console.WriteLine("Task4"); 
 
             //Task4_6Part1 task4_6Part1 = new Task4_6Part1();
             //string a = Console.ReadLine();
@@ -56,7 +56,65 @@ namespace MyApp // Note: actual namespace depends on the project name.
             //{
             //    task4_6Part1.Task4(a);
             //}
+            //5. ПЕРЕПУТАЛ они в другой очередности Напишите программу, которая запрашивает у пользователя длину и ширину
+            //прямоугольника, а затем выводит его площадь.
+            //Console.WriteLine("Task5");
+
+            //Task4_6Part1 task4_6Part1 = new Task4_6Part1();
+            //string a = Console.ReadLine();
+            //string f = Console.ReadLine();
+            //bool b = int.TryParse(a, out int c);
+            //bool d = int.TryParse(f, out int e);
+
+            //if (b && d && c != null && e != null)
+            //{
+            //    Console.WriteLine(task4_6Part1.Task5(c,e));
+            //}
+
+            //6. Напишите программу, которая запрашивает у пользователя три числа и выводит
+            //на экран наименьшее из них.
+            //Console.WriteLine("Task6");
+
+            //Task4_6Part1 task4_6Part1 = new Task4_6Part1();
+            //string a = Console.ReadLine();
+            //string f = Console.ReadLine();
+            //string g = Console.ReadLine();
+            //bool b = int.TryParse(a, out int c);
+            //bool d = int.TryParse(f, out int e);
+            //bool i = int.TryParse(g, out int k);
+
+            //if (b && d && c != null && e != null)
+            //{
+            //    Console.WriteLine(task4_6Part1.Task6(c, e,k));
+            //}
+            //7. Напишите программу, которая запрашивает у пользователя строку и выводит на
+            //экран количество гласных букв в этой строке.
+
+            //Console.WriteLine("Task7");
+
+            //Task7_9Part1 task7_9Part1 = new Task7_9Part1();
+            //string a = Console.ReadLine();
             
+
+            //if (a != null )
+            //{
+            //    Console.WriteLine(task7_9Part1.Task7(a));
+            //}
+
+
+            //8. Напишите программу, которая запрашивает у пользователя год рождения и
+            //выводит его возраст на текущую дату.
+            Console.WriteLine("Task8");
+
+            Task7_9Part1 task7_9Part1 = new Task7_9Part1();
+
+            string a = Console.ReadLine();
+            bool b = int.TryParse(a, out int c);
+
+            if (b && c != null)
+            {
+                task7_9Part1.Task8(c);
+            }
         }
     }
 }
