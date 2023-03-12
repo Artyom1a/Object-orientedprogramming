6153920a92e37d0ad2594719fd481eef40838520
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-12 20:01:36 +0300

    12.03

---
+++ OOP13.03/ConsoleApp1/Task10_12Part1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Task10_12Part1
+    {
+        public void Task10()
+        {
+            int a = new Random().Next(1, 100);
+            Console.WriteLine($"random number a= {a}");
+            Console.WriteLine("Write number b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("random number >your number");
+                    b = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("random number <your number");
+                    b = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            if (a == b)
+            {
+                Console.WriteLine("random number ==your number");
+            }
+        }
+    }
+}
