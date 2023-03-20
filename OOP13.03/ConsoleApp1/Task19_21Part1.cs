342bc43df18021b6fbc377cd6787ce0f6af435b4
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-20 13:42:25 +0300

    20.03

---
+++ OOP13.03/ConsoleApp1/Task19_21Part1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Task19_21Part1
+    {
+
+        public bool Task19(string a)
+        {
+            List<char> lista = new List<char>();
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                lista.Add(a[i]);
+            }
+
+            List<char> listb = new List<char>();
+            for (int j = 0; j < a.Length; j++)
+            {
+                listb.Add(a[j]);
+            }
+
+
+            if (lista.SequenceEqual(listb))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Task20(string a)
+        {
+            a = a.Replace(" ", "");
+            Console.WriteLine("String without probels: " + a);
+        }
+    }
+}
