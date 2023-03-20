8dc3a18e033d1d4f542d6a24133fa977a659355d
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-03-20 13:41:55 +0300

    20.03

---
+++ OOP13.03/ConsoleApp1/Task16_18Part1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Task16_18Part1
+    {
+
+
+
+
+
+
+
+
+
+        public List<char> Task18(string a)
+        {
+            List<char> result = new List<char>();
+            for(int i= a.Length-1; i>=0;i--)
+            {
+                result.Add(a[i]);
+            }
+
+            return result; 
+        }
+    }
+}
