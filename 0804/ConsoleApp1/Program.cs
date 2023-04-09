459a2cb27a8158622cbcc064527aabafa46828b9
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-09 22:58:11 +0300

    feature0904n1

---
+++ 0804/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            1.
+            //Напишите программу, которая создает 10 задач и запускает их параллельно.
+            //Каждая задача должна выводить в консоль свой порядковый номер.
+            
+            List<Task> tasks = new List<Task>();
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(TaskMethod(i));
+
+            }
+            Task.WaitAll(tasks.ToArray());
+
+
+        }
+        static async Task TaskMethod(int number)
+        {
+            Console.WriteLine($"Method  {number} stating...");
+            await Task.Delay(100);
+            Console.WriteLine($"Method  {number} finished...");
+        }
+    }
+}
