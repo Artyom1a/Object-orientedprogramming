34da9acef5748a1a33a803f1ed97842b9ad0c01d
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-12 22:28:42 +0300

    1204

---
+++ 1004/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
+//1.
+//Создайте класс Animal с полем name. Добавьте метод makeSound(), который выводит на
+//консоль звук, издаваемый животным. Создайте классы Dog и Cat, которые наследуются от
+//Animal и переопределяют метод MakeSound() для воспроизведения соответствующих звуков.
+
+
+//Создайте интерфейс Drawable, который имеет метод Draw(). Создайте классы Circle, Square и
+//Triangle, которые реализуют интерфейс Drawable и реализуют метод draw() для отрисовки
+//соответствующих фигур.
