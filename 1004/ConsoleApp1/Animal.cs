70a1d2bd4ad035abbe3dcb135189a569b8ea478c
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-12 22:26:43 +0300

    1204

---
+++ 1004/ConsoleApp1/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Animal
+    {
+
+        public string Name;
+
+        public virtual void makeSound()
+        {
+            Console.WriteLine("sound");
+        }
+    }
+}
+//1.
+//Создайте класс Animal с полем name. Добавьте метод makeSound(), который выводит на
+//консоль звук, издаваемый животным. Создайте классы Dog и Cat, которые наследуются от
+//Animal и переопределяют метод MakeSound() для воспроизведения соответствующих звуков.
