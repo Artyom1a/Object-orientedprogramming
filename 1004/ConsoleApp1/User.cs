774051546a336eebab71dc3c5c2130192fd39cf5
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-14 23:16:23 +0300

    1404

---
+++ 1004/ConsoleApp1/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public User(int id, string name) => (ID, Name)=(id, name);
+  
+        public int ID { get; set; }
+        public string Name { get; set; }
+       
+    }
+}
