3ccfb09964bb1ac62d2b5ff5b5f6f5ab373955e1
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-14 23:11:46 +0300

    1404

---
+++ 1004/ConsoleApp1/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Square : IDrawable
+    {
+
+      
+            public void Draw()
+            {
+                throw new NotImplementedException();
+            }
+        
+    }
+}
