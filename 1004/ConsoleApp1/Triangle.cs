4a060e7c22710ed48f1866556d7cbfb5b1fdc7ab
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-14 23:13:39 +0300

    1404

---
+++ 1004/ConsoleApp1/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Triangle : IDrawable
+    {
+
+
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
